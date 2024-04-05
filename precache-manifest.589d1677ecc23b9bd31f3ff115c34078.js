83fb4d10a2d1e5e0d48b21c1af5f13c480519439
Namrata <namrata@Namratas-MacBook-Air-2.local>
2024-04-05 14:50:08 +0530

    try

---
+++ precache-manifest.589d1677ecc23b9bd31f3ff115c34078.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebb653d2ed3f13363e28175211c6d6f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75e14d297db37c0be021",
+    "url": "/static/css/main.a4bf516a.chunk.css"
+  },
+  {
+    "revision": "05abd69420b9e89a9866",
+    "url": "/static/js/2.c2620cbf.chunk.js"
+  },
+  {
+    "revision": "75e14d297db37c0be021",
+    "url": "/static/js/main.95571b82.chunk.js"
+  },
+  {
+    "revision": "fe5c7aae141f03b42984",
+    "url": "/static/js/runtime-main.8eea71de.js"
+  },
+  {
+    "revision": "487607d8e75542ea66b46c5cda282f50",
+    "url": "/static/media/hey.487607d8.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "639abfed7b76b5ab333b708c85a0dcc0",
+    "url": "/static/media/story.639abfed.png"
+  }
+]);
